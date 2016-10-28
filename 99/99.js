455689d4a1ced5b0349a0ea2fdff70ee87b9be18
Shota Shimazu <hornet.live.mf@gmail.com>
2016-10-28 14:39:44 +0900

    Add 99.

--- 99/99.js
+++ 99/99.js
@@ -1,6 +1,7 @@
 for (var i = 1; i < 10; i++) {
     for (var j = 1; j < 10; j++) {
         document.getElementById("res").innerHTML +=  "<p class=\"piece\">" + i*j   + "</p>";
+       // document.getElementById("res").innerHTML += "<br>";
     }
-    document.getElementById("res").innerHTML += "<br>";
+    document.getElementById("res").innerHTML += "<br><br>";
 }
