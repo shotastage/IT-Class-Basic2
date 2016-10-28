92cc52941646c3b8c3ecf81d5089175543768aa9
Shota Shimazu <shotastage@users.noreply.github.com>
2016-10-28 12:43:13 +0900

    Create 99.js

---
+++ 99.js
@@ -0,0 +1,6 @@
+for (var i = 1; i < 10; i++) {
+    for (var j = 1; j < 10; j++) {
+        document.getElementById("res").innerHTML +=  "<p class=\"piece\">" + i*j   + "</p>";
+    }
+    document.getElementById("res").innerHTML += "<br>";
+}
