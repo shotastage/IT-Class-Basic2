383595eb58201dca2c7748196fcaf5f41e6208fa
Shota Shimazu <shotastage@users.noreply.github.com>
2016-10-28 14:42:59 +0900

    Update 99.js

--- 99/99.js
+++ 99/99.js
@@ -1,7 +1,9 @@
 for (var i = 1; i < 10; i++) {
+    
     for (var j = 1; j < 10; j++) {
         document.getElementById("res").innerHTML +=  "<p class=\"piece\">" + i*j   + "</p>";
-       // document.getElementById("res").innerHTML += "<br>";
     }
+    
+    // 表を改行
     document.getElementById("res").innerHTML += "<br><br>";
 }
